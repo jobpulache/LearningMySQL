08272610a0d48c0aae8eeef62f3e207899ebd9c3
Job Pulache <jobpulachecarrenojob837@gmail.com>
2025-01-02 21:27:52 -0500

    join two tablets or more

--- advance.sql
+++ advance.sql
@@ -96,7 +96,8 @@ INSERT INTO CitasServicios(CitasId, ServiciosId) VALUES(1,3);
 #Set information in table pivote
 SELECT * FROM CitasServicios 
 LEFT JOIN citas ON citas.id = CitasServicios.CitasId
-LEFT JOIN clientes ON citas.clienteId = clientes.id
+LEFT JOIN clientes ON citas.clienteId	 = clientes.id
 LEFT JOIN servicios ON servicios.id = CitasServicios.ServiciosId;
 INSERT INTO CitasServicios(	CitasId, ServiciosId) VALUES(2, 8);
 select * from CitasServicios;
+
